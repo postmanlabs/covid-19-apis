3e795803e05eb5e67cc6053db862c76b832c4d86
Christina <christinavhastenrath@gmail.com>
2020-03-23 15:06:58 -0700

    added hero and header

--- src/components/Shared/Hero.jsx
+++ src/components/Shared/Hero.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+// import heroImage from '../../images/hero_BG-asset.svg';
+// import heroPostmanaut from '../../images/hero-postmanaut.svg';
+import heroPostmanaut from '../../images/covid19-image-2.png';
+
 
 const Hero = () => (
-  <div className="container-fluid">
-    {/* <img src="" */}
-    <h1> HERO</h1>
+  <div className="container-fluid hero">
+    <div className="row">
+      <div className="col-xs-6 col-lg-6 hero_title">
+        <h1>
+          Postman
+          <br />
+          Resources
+          <br />
+          for COVID-19
+        </h1>
+      </div>
+      <div className="col-xs-6 col-lg-6">
+        <img src={heroPostmanaut} alt="postman hero" />
+      </div>
+    </div>
+
   </div>
 );
 
