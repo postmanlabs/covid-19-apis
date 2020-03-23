cc38a9882b576cf7947003337096f0a8bda037f5
Christina <christinavhastenrath@gmail.com>
2020-03-23 13:11:21 -0700

    wip - pulling in dev

---
+++ src/components/Shared/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => (
+  <div className="container-fluid">
+    {/* <img src="" */}
+    <h1> HERO</h1>
+  </div>
+);
+
+
+export default Hero;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,10 +4,15 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
+import Hero from '../components/Shared/Hero';
+import Welcome from '../components/Shared/Welcome';
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Hero />
+    <Welcome />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
