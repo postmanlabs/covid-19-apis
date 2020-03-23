156d8096ba73ad3f282cf8ae11d7d92ed88af347
Christina <christinavhastenrath@gmail.com>
2020-03-23 11:24:11 -0700

    wip - pulling in dev

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: 'rebeccapurple',
+      background: '#2B3B41',
       marginBottom: '1.45rem',
     }}
   >
