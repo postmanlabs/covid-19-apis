f1e710b41a3386392dbd9013ce43d9ab8def1635
Christina <christinavhastenrath@gmail.com>
2020-03-23 10:56:15 -0700

    added footer

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const uuidv4 = require('uuid/v4');
 
-// You can delete this file if you're not using it
+// const HeaderJson = require('./src/components/Header/Header.data.json');
+const FooterJson = require('./src/components/Footer/Footer.data.json');
+
+
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const prepareNode = (obj, name) => {
+    const data = {
+      key: uuidv4(),
+      value: JSON.stringify(obj),
+    };
+    const node = JSON.stringify(data);
+    const nodeMeta = {
+      id: createNodeId(`my-data-${data.key}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: name,
+        mediaType: 'text/json',
+        content: node,
+        contentDigest: createContentDigest(data),
+      },
+    };
+
+    const output = { ...data, ...nodeMeta };
+    return output;
+  };
+
+  const { createNode } = actions;
+
+  // createNode(prepareNode(HeaderJson, 'headerLinks'));
+  createNode(prepareNode(FooterJson, 'FooterLinks'));
+};
