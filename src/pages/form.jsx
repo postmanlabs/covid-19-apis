e8a9ef5bde81103bc36746fda163e0d930b4836d
Christina <christinavhastenrath@gmail.com>
2020-03-25 14:39:48 -0700

    wip form

---
+++ src/pages/form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const FormPage = () => {
+  <Layout>
+    <SEO title="Contact Postman Form" />
+
+  </Layout>
+}
+
+export default FormPage;
