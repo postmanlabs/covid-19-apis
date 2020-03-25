a560b91982cb0d888f4b63c08935c578a54f3b58
Ryan-McBride <mcbrideryan89@gmail.com>
2020-03-24 17:03:21 -0700

    removed conditional

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,28 +1,25 @@
 exports.onClientEntry = () => {
-  console.log('client is here');
-  if (typeof document === 'object') {
-    (() => {
-      let initializedForm = false;
+  (() => {
+    let initializedForm = false;
 
-      const loadForm = () => {
-        if (initializedForm === false) {
-          initializedForm = true;
-          window.MktoForms2.loadForm('//pages.getpostman.com', '067-UMD-991', 1376);
-        }
-      };
-      const s = document.createElement('script');
+    const loadForm = () => {
+      if (initializedForm === false) {
+        initializedForm = true;
+        window.MktoForms2.loadForm('//pages.getpostman.com', '067-UMD-991', 1376);
+      }
+    };
+    const s = document.createElement('script');
 
-      s.type = 'text/javascript';
-      s.async = true;
-      s.src = '//pages.getpostman.com/js/forms2/js/forms2.min.js';
-      s.onreadystatechange = () => {
-        if (this.readyState === 'complete' || this.readyState === 'loaded') {
-          loadForm();
-        }
-      };
-      s.onload = loadForm;
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = '//pages.getpostman.com/js/forms2/js/forms2.min.js';
+    s.onreadystatechange = () => {
+      if (this.readyState === 'complete' || this.readyState === 'loaded') {
+        loadForm();
+      }
+    };
+    s.onload = loadForm;
 
-      document.getElementsByTagName('head')[0].appendChild(s);
-    })();
-  }
+    document.getElementsByTagName('head')[0].appendChild(s);
+  })();
 };
