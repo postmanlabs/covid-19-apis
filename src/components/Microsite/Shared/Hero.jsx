7da0b99afa38c95047f31c8625ee90ee3c8bfac9
tristandenyer <tristan@tristandenyer.com>
2020-03-26 15:02:14 -0700

    [69] refactored grid

--- src/components/Microsite/Shared/Hero.jsx
+++ src/components/Microsite/Shared/Hero.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 // import heroImage from '../../images/hero_BG-asset.svg';
 // import heroPostmanaut from '../../images/hero-postmanaut.svg';
-import heroPostmanaut from '../../../images/covid19-image-2.png';
+import heroImage from '../../../images/covid19-image-2.png';
 
 
 const Hero = () => (
-  <div className="container-fluid hero v5_starfield-small-night-sky">
-    <div className="row">
-      <div className="col-xs-6 col-lg-6 hero_title">
-        <h1>
-          Postman
-          <br />
-          COVID-19 API
-          <br />
-          Resource Center
-        </h1>
-      </div>
-      <div className="col-xs-6 col-lg-6">
-        <img src={heroPostmanaut} alt="postman hero" />
+  <div className="container-fluid hero v5_starfield-small-night-sky pb-4">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 hero_title">
+          <h1>
+            Postman COVID-19 API Resource Center
+          </h1>
+        </div>
+        <div className="col-md-6 hero_image">
+          <img src={heroImage} alt="Isolated COVID-19 virus in space. Illustration." />
+        </div>
       </div>
     </div>
   </div>
