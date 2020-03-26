9aa9fb037a22db0eddebb226d6e1a4df1c64095b
Christina <christinavhastenrath@gmail.com>
2020-03-26 09:59:15 -0700

    excluded urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,12 @@ module.exports = {
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sitemap',
+    {
+      resolve:  'gatsby-plugin-sitemap',
+      options: {
+        exclude: ['/thankyou/', '/frontmatter', '/content/*']
+      },
+    },
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
