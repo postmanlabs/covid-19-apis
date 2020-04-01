f0d80b71f9d5b0d2e0eb94d6a4c3e918c9e97712
tristandenyer <tristan@tristandenyer.com>
2020-03-31 17:34:17 -0700

    updated styling

--- src/components/TestingSites/callToActionConsumers.jsx
+++ src/components/TestingSites/callToActionConsumers.jsx
@@ -3,15 +3,13 @@ import { Link } from 'gatsby';
 
 const callToActionConsumers = () => (
   <div className="container-fluid ts-section">
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-12 ts-call-to-action">
-          <h2>Know of Updated Testing Sites?</h2>
-          <p>
-            This is a crowdsourced project. If you found a new or updated testing site and you’d like to submit it, email us.
-          </p>
-          <Link to="mailto:covid-19@postman.com" className="btn btn-dark ts-button">Email us</Link>
-        </div>
+    <div className="row">
+      <div className="col-sm-12 ts-call-to-action">
+        <h2>Know of Updated Testing Sites?</h2>
+        <p>
+          This is a crowdsourced project. If you found a new or updated testing site and you’d like to submit it, email us.
+        </p>
+        <Link to="mailto:covid-19@postman.com" className="btn btn-dark ts-button">Email us</Link>
       </div>
     </div>
   </div>
