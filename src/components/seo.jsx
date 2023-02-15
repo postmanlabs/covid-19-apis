793ed308e9f8143815c911f0c42cfd34d20e7f12
Christina Hastenrath <christina.hastenrath@postman.com>
2023-02-15 11:15:59 -0800

    fixed linter errors

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -96,7 +96,7 @@ function SEO({
       {/* OneTrust */}
       <script type="text/javascript" src="https://cdn.cookielaw.org/consent/1cef3369-6d07-4928-b977-2d877eb670c4/OtAutoBlock.js" />
 
-      <script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" type="text/javascript" charset="UTF-8" data-domain-script="1cef3369-6d07-4928-b977-2d877eb670c4" />
+      <script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" type="text/javascript" data-domain-script="1cef3369-6d07-4928-b977-2d877eb670c4" />
       {/* fonts */}
       <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap" rel="stylesheet" />
