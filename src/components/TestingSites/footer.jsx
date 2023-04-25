b18f14f7b69137eb8744ca0626dd8dd0ea68f5de
OlliesWorld <rtlockwoodwork@gmail.com>
2023-04-25 09:52:14 -0600

    update testing folder with footer

--- src/components/TestingSites/footer.jsx
+++ src/components/TestingSites/footer.jsx
@@ -1,51 +1,293 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-useless-fragment */
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import footerDataLocal from '../../../build/footerDev.json';
+import footerData from '../../../bff-data/footer.json';
+
+const triggerGA = (category, label) => (
+  category
+  && label
+  && window.pm
+  && window.pm.ga('send', 'event', category, 'Click', label)
+);
+
+// Helper function for rel attribute in link or button
+function relStringGenerator(target) {
+  if (target === '') {
+    return null;
+  }
+  if (target === 'blank') {
+    return 'noopener';
+  }
+  return null;
+}
+// // Helper function for target attribute in link or button
+function targetStringGenerator(target) {
+  if (target === '') {
+    return null;
+  }
+  if (target === 'blank') {
+    return '_blank';
+  }
+  return null;
+}
 
 function Footer() {
+  const [data, setData] = useState(footerData);
+  // runtime check to switch between prod and local data if API returns malformed
+  const footerKeys = ['alt', 'copyright', 'items', 'src', 'type'];
+  useEffect(() => {
+    if (footerKeys.every((key) => Object.keys(footerData).includes(key))) {
+      setData(footerData);
+    } else {
+      setData(footerDataLocal);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const columns = data.items.slice(0, 5);
+
   return (
-    <div className="container-fluid ts-footer ts-section">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12 mb-4">
-            <a className="homelink" href="/covid-19-testing-locations/">COVID-19 Testing Locations</a>
-            <a className="navlink" href="/covid-19-testing-locations/about/">About</a>
-            <a className="navlink" href="/covid-19-testing-locations/faqs/">FAQs</a>
-            <a className="navlink" href="/covid-19-testing-locations/how-it-works/">How it Works</a>
-            <a className="navlink" href="/covid-19-testing-locations/legal/">Legal</a>
-            <a href="https://github.com/postmanlabs/covid-19-apis" className="align-middle">
-              <span className="ts-icon">
-                <svg className="navbar-nav-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 499.36" focusable="false">
-                  <title>GitHub</title>
-                  <path d="M256 0C114.64 0 0 114.61 0 256c0 113.09 73.34 209 175.08 242.9 12.8 2.35 17.47-5.56 17.47-12.34 0-6.08-.22-22.18-.35-43.54-71.2 15.49-86.2-34.34-86.2-34.34-11.64-29.57-28.42-37.45-28.42-37.45-23.27-15.84 1.73-15.55 1.73-15.55 25.69 1.81 39.21 26.38 39.21 26.38 22.84 39.12 59.92 27.82 74.5 21.27 2.33-16.54 8.94-27.82 16.25-34.22-56.84-6.43-116.6-28.43-116.6-126.49 0-27.95 10-50.8 26.35-68.69-2.63-6.48-11.42-32.5 2.51-67.75 0 0 21.49-6.88 70.4 26.24a242.65 242.65 0 0 1 128.18 0c48.87-33.13 70.33-26.24 70.33-26.24 14 35.25 5.18 61.27 2.55 67.75 16.41 17.9 26.31 40.75 26.31 68.69 0 98.35-59.85 120-116.88 126.32 9.19 7.9 17.38 23.53 17.38 47.41 0 34.22-.31 61.83-.31 70.23 0 6.85 4.61 14.81 17.6 12.31C438.72 464.97 512 369.08 512 256.02 512 114.62 397.37 0 256 0z" fill="#DDDDDD" fillRule="evenodd" />
-                </svg>
-              </span>
-            </a>
+    <div className="footer">
+      <section id="Footer" className="section">
+        <div className="container-footer">
+          <div className="row">
+            <div className="col-sm-8 offset-sm-2 col-md-12 offset-md-0">
+              <div className="row">
+                {/* Copyright */}
+                <div className="FooterImgWrapper col-8 offset-2 col-md-3 offset-md-0 col-lg-2 order-12 order-md-0 pad-md-right align-self-center">
+                  <img className="footer-img mb-5" src="https://voyager.postman.com/illustration/postman-footer-rocket-launch.svg" alt="Postman" />
+                  <span className="col-12 d-none d-md-block copyright">
+                    {data.copyright}
+                  </span>
+                </div>
+                {/* Product */}
+                <div className="column col-2 offset-md-1  offset-lg-2 order-1 order-md-2 mb-5">
+                  {columns.slice(0, 1).map((item) => (
+                    <nav aria-labelledby={item.arialabelledby} key={uuidv4()}>
+                      <h2 className="footer-col-title" id={item.arialabelledby}>
+                        {item.title}
+                      </h2>
+                      <div className="column">
+                        {(item.items
+                          && item.items.map((link) => (
+                            <li className="column-row" key={uuidv4()}>
+                              <a
+                                className="column-link"
+                                id={link.id}
+                                href={link.url}
+                                rel={relStringGenerator(link.target)}
+                                target={targetStringGenerator(link.target)}
+                                onClick={() => {
+                                  triggerGA(link.category, link.label);
+                                }}
+                              >
+                                {link.span ? (
+                                  <>
+                                    {link.title}
+                                    <span>{link.span}</span>
+                                  </>
+                                ) : (
+
+                                  <>{link.title}</>
+                                )}
+                              </a>
+                            </li>
+                          )))
+                          || ''}
+                      </div>
+                    </nav>
+                  ))}
+                </div>
+                <div className="column col-2 order-2 order-md-3">
+                  {/* Company - stacked top */}
+                  {columns.slice(1, 2).map((item) => (
+                    <div key={uuidv4()}>
+                      <nav
+                        aria-labelledby={item.arialabelledby}
+                        className="mb-5"
+                      >
+                        <h2 className="footer-col-title" id={item.arialabelledby}>
+                          {item.title}
+                        </h2>
+                        <div className="column">
+                          {(item.items
+                            && item.items.map((link) => (
+                              <li className="column-row" key={uuidv4()}>
+                                <a
+                                  className="column-link"
+                                  id={link.id}
+                                  href={link.url}
+                                  rel={relStringGenerator(link.target)}
+                                  target={targetStringGenerator(link.target)}
+                                  onClick={() => {
+                                    triggerGA(link.category, link.label);
+                                  }}
+                                >
+                                  {link.span ? (
+                                    <>
+                                      {link.title}
+                                      <span>{link.span}</span>
+                                    </>
+                                  ) : (
+
+                                    <>{link.title}</>
+                                  )}
+                                </a>
+                              </li>
+                            )))
+                            || ''}
+                        </div>
+                      </nav>
+                    </div>
+                  ))}
+                  {/* Company stacked bottom */}
+                  {columns.slice(2, 3).map((item) => (
+                    <div key={uuidv4()}>
+                      <nav
+                        aria-labelledby={item.arialabelledby}
+                        className="mb-5"
+                      >
+                        <h2 className="footer-col-title" id={item.arialabelledby}>
+                          {item.title}
+                        </h2>
+                        <div className="column">
+                          {(item.items
+                            && item.items.map((link) => (
+                              <li className="column-row" key={uuidv4()}>
+                                <a
+                                  className="column-link"
+                                  id={link.id}
+                                  href={link.url}
+                                  rel={relStringGenerator(link.target)}
+                                  target={targetStringGenerator(link.target)}
+                                  onClick={() => {
+                                    triggerGA(link.category, link.label);
+                                  }}
+                                >
+                                  {link.span ? (
+                                    <>
+                                      {link.title}
+                                      <span>{link.span}</span>
+                                    </>
+                                  ) : (
+                                    <>{link.title}</>
+                                  )}
+                                </a>
+                              </li>
+                            )))
+                            || ''}
+                        </div>
+                      </nav>
+                    </div>
+                  ))}
+                </div>
+                {/* API Categories */}
+                <div className="column col-2 order-3  order-md-4">
+                  {columns.slice(3, 4).map((item) => (
+
+                    <nav aria-labelledby={item.arialabelledby} key={uuidv4()}>
+                      <h2 className="footer-col-title" id={item.arialabelledby}>
+                        {item.title}
+                      </h2>
+                      <div className="column">
+                        {(item.items
+                          && item.items.map((link) => (
+                            <li className="column-row" key={uuidv4()}>
+                              <a
+                                className="column-link"
+                                id={link.id}
+                                href={link.url}
+                                rel={relStringGenerator(link.target)}
+                                target={targetStringGenerator(link.target)}
+                                onClick={() => {
+                                  triggerGA(link.category, link.label);
+                                }}
+                              >
+                                {link.span ? (
+                                  <>
+                                    {link.title}
+                                    <span>{link.span}</span>
+                                  </>
+                                ) : (
+                                  <>{link.title}</>
+                                )}
+                              </a>
+                            </li>
+                          )))
+                          || ''}
+                      </div>
+                    </nav>
+                  ))}
+
+                </div>
+                {/* Social media icons */}
+                <div className="column__social order-4 col-lg-2 order-md-5">
+
+                  {columns.slice(4, 5).map((item) => (
+                    <div key={uuidv4()}>
+                      <nav
+                        aria-labelledby={item.arialabelledby}
+                        className="mb-5 mr-0"
+                      >
+                        <h2 className="footer-col-title" id={item.arialabelledby}>
+                          {item.title}
+                        </h2>
+                        <div className="column">
+                          {(item.items
+                              && item.items.map((link) => (
+                                <li className="column-row" key={uuidv4()}>
+                                  <a
+                                    className="column-link"
+                                    id={link.id}
+                                    href={link.url}
+                                    rel={relStringGenerator(link.target)}
+                                    target={targetStringGenerator(
+                                      link.target,
+                                    )}
+                                    onClick={() => {
+                                      triggerGA(link.category, link.label);
+                                    }}
+                                  >
+                                    <div direction="row" wrap="nowrap">
+                                      <div
+                                        className="svg d-inline-block align-self-center social-svg"
+                                        // eslint-disable-next-line react/no-danger
+                                        dangerouslySetInnerHTML={{
+                                          __html: link.svg,
+                                        }}
+                                      />
+                                      {link.span ? (
+                                        <>
+                                          {link.title}
+                                          <span>{link.span}</span>
+                                        </>
+                                      ) : (
+                                        <div className="d-inline-block align-self-center">
+                                          {link.title}
+                                        </div>
+                                      )}
+                                    </div>
+                                  </a>
+                                </li>
+                              )))
+                              || ''}
+                        </div>
+                      </nav>
+                    </div>
+                  ))}
+
+                </div>
+                <div className="col-12 d-block d-md-none text-center order-last">
+                  <span className="col-12 copyright">{data.copyright}</span>
+                </div>
+              </div>
+            </div>
           </div>
+          {/*  eslint-enable */}
         </div>
-        <div className="row attribution">
-          <div className="col-sm-12">
-            <a href="https://www.postman.com/" target="_blank" rel="noopener noreferrer"><img className="pm-logo" src="https://assets.getpostman.com/common-share/postman-logo-horizontal-white.svg" alt="Postman" /></a>
-            <p className="mb-4">
-              A project developed by the
-              {' '}
-              <a href="https://www.postman.com/" className="attributionlink" target="_blank" rel="noopener noreferrer">Postman team</a>
-              , using APIs from the
-              {' '}
-              <a href="https://covid-19-apis.postman.com/" className="attributionlink" target="_blank" rel="noopener noreferrer">Postman COVID-19 API Resource Center</a>
-              , along with
-              {' '}
-              <a href="https://github.com/postmanlabs/covid-19-apis" className="attributionlink" target="_blank" rel="noopener noreferrer">community contributions</a>
-              .
-            </p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-12 copyright">
-            <span className="align-middle mb-0">
-              &copy; 2022 Postman, Inc. All rights reserved.
-            </span>
-          </div>
-        </div>
-      </div>
+      </section>
     </div>
   );
 }
