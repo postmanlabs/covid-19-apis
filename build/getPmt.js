354eb0c8db83a38576d9e2c0dc91b96afea44d9f
oswish <56083362+oswish@users.noreply.github.com>
2024-08-20 15:57:51 -0700

    [MARKENG-3550] update to use latest published pmt on build, add "change" npm script, remove fingerPrintJS, use identity url for sign-in (#433)
    * [MARKENG-3550][c] update to use latest published pmt on build
    
    * [MARKENG-3558][c] add "change" npm script
    
    * [MARKENG-3559][c] remove fingerPrintJS
    
    * [MARKENG-3560][c] use identity url for sign-in
    
    ---------
    
    Co-authored-by: Brian Swisher <brian.swisher@postman.com@Brians-MacBook-Pro.local>

---
+++ build/getPmt.js
@@ -0,0 +1,39 @@
+const https = require('follow-redirects').https;
+const fs = require('fs');
+const path = require('path');
+const sh = require('shelljs');
+
+sh.config.silent = true;
+
+sh.exec('mkdir -p public');
+
+const TIME = new Date().getTime();
+
+const options = {
+  method: 'GET',
+  hostname: 'www.postman.com',
+  path: `/mkapi/pmt.js?${TIME}`,
+  maxRedirects: 20,
+};
+
+const req = https.request(options, function (res) {
+  const chunks = [];
+
+  res.on('data', function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on('end', function () {
+    const body = Buffer.concat(chunks);
+
+    fs.writeFile(path.join('pmt.js'), body.toString(), (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+
+    sh.config.silent = false;
+  });
+});
+
+req.end();
